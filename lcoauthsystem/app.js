46903d5ea49af91dccf1291d10893f3061fc9e03
Yash Patel <62371168+yashpatel4900@users.noreply.github.com>
2022-07-29 14:01:32 +0530

    PORT Variable

---
+++ lcoauthsystem/app.js
@@ -0,0 +1,11 @@
+require("dotenv").config();
+const express = require("express");
+
+const app = express();
+
+app.get("/", (req, res) => {
+  res.send("<h1>Hello from Auth System - LCO</h1>");
+});
+
+// Instead of writing all listen statements in app.js we will write them in index.js
+module.exports = app;
