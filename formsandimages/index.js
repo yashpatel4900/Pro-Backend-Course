0ae53db380e7c1b540b1254137e7f80dfce99984
Yash Patel <62371168+yashpatel4900@users.noreply.github.com>
2022-08-03 14:53:06 +0530

    Handling and Uploading a Single File

--- formsandimages/index.js
+++ formsandimages/index.js
@@ -1,13 +1,23 @@
 const express = require("express");
 const fileUpload = require("express-fileupload");
+const cloudinary = require("cloudinary").v2;
 
 const app = express();
 
+cloudinary.config({
+  // cloud_name : process.env.CLOUD_NAME
+  cloud_name: "yashpatel4900",
+  api_key: "284557364345656",
+  api_secret: "y7zL1ni-Py8rkcxi66N5VevgH4k",
+});
+
 app.set("view engine", "ejs");
 app.use(express.json());
+
+// Creates temporary zone where our file which is in a queue of being uploaded will stay for a while
 app.use(
   fileUpload({
-    useTemplate: true,
+    useTempFiles: true,
     tempFileDir: "/tmp/",
   })
 );
@@ -20,10 +30,28 @@ app.get("/myget", (req, res) => {
   res.send(req.query);
 });
 
-app.post("/mypost", (req, res) => {
+app.post("/mypost", async (req, res) => {
   console.log(req.body);
   console.log(req.files);
-  res.send(req.body);
+
+  // Grabbing file comming from form
+  let file = req.files.sampleFile;
+
+  // Use exact keyword 'result' according to docs as cloudinary will return some data after upload
+  // according to docs - cloudinary.v2.uploader.upload(file, options, callback);
+  result = await cloudinary.uploader.upload(file.tempFilePath, {
+    folder: "users",
+  });
+
+  console.log(result);
+
+  // Setting up details of what we have got from POST API
+  details = {
+    firstname: req.body.firstname,
+    lastname: req.body.lastname,
+    result,
+  };
+  res.send(details);
 });
 
 app.get("/mygetform", (req, res) => {
